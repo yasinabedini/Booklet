2dce14e0384175e8f158f8d2d164125bee8d3fc5
yasin abedini <yasinabedini1384@gmail.com>
2023-06-01 17:45:41 +0330

    Add project files.

---
+++ Advance C #/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advance_C__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
